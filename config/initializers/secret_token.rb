4afdec871bf7b0b843f415faad1091fa8f55fa36
Adam Rice <radrice@gmail.com>
2014-02-25 19:32:32 -0500

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RobotSchool::Application.config.secret_key_base = 'ef1f478b6cade9948caa45ab3f78d6fefadfe5f2ad5a6fb265fc999ab8a77dd57ce8749b348bc2577036faf69ca799488671e3d8ee83d1fd71b74ffb1a63939a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RobotSchool::Application.config.session_store :cookie_store, key: '_robot_school_session'
